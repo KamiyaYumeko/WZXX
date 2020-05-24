ce3f3818e0073e6bba0eb756238ece6d72611454
xdy438 <798304307@qq.com>
2020-05-24 14:08:14 +0800

    添加物品名字和图片更换

--- Assets/Scripts/game/Equip.cs
+++ Assets/Scripts/game/Equip.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 //物品
 public class Equip : MonoBehaviour
@@ -8,6 +9,11 @@ public class Equip : MonoBehaviour
     public int Index;                               //物品ID
 
     public int Type;                                //物品类型 1装备 2消耗品 3特殊
-    
 
+    public void ImgAndNum(int index,int num)
+    {
+        transform.GetComponentInChildren<Text>().text = ""+num;
+        Sprite sprite = Resources.Load("", typeof(Sprite)) as Sprite;    //参数为资源路径和资源类型
+        transform.GetComponent<Image>().sprite = sprite;
+    }
 }
