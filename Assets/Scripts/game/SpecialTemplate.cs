40dd7c2f957625fa5bd2d27a06ea091122ccffa3
xdy438 <798304307@qq.com>
2020-05-29 16:30:38 +0800

    增加特殊品模板

---
+++ Assets/Scripts/game/SpecialTemplate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//特殊品模板
+public class SpecialTemplate
+{
+    public int Id;
+
+    public string Name;
+
+    public string Information;
+
+    public int Type;
+}
